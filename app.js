e55b19c08a857caf20833c0b700c450534f25493
ambernluu <ambernluu@gmail.com>
2022-05-27 09:56:26 -0400

    final

---
+++ app.js
@@ -0,0 +1,27 @@
+const api_key = 'fPGB2SHqZe8j47vQ7ES6cRBKKxCcE18m';
+const search = document.getElementById('search-box');
+const form = document.getElementById('form-box');
+const removeBtn = document.getElementById('remove');
+const gifBox = document.getElementById('gif-box');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const q = search.value;
+  getGif(q);
+});
+removeBtn.addEventListener('click', (e) => {
+  console.log(`i click i click`);
+  gifBox.replaceChildren();
+});
+
+async function getGif(q) {
+  const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, {
+    params: { q, api_key },
+  });
+
+  const rand = Math.floor(Math.random() * 50);
+  const gif = document.createElement('img');
+  gif.className = 'gifs';
+  gif.src = res.data.data[Math.floor(Math.random() * 50)].images.original.url;
+  gifBox.append(gif);
+}
